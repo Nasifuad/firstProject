d6d6086f7dcacf15bf93ed42bf299e4e6c3b1714
Nasif Fuad <nasifuad007@gmail.com>
2025-03-21 22:18:49 +0600

    linked list in py

---
+++ Class.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current is not None:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+        
+
+llist = LinkedList()
+llist.insert(1)
+llist.insert(2)
+llist.insert(3)
+
+llist.display()
